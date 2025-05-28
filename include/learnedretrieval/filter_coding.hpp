2bb50f57a20417bae6521ebf60715fa7895786a9
Stefan Hermann <stefan.a.hermann@gmail.com>
2025-05-28 19:47:03 +0200

    filter now works, no tuning so far

--- include/learnedretrieval/filter_coding.hpp
+++ include/learnedretrieval/filter_coding.hpp
@@ -7,47 +7,194 @@
 #include <vector>
 #include <ranges>
 #include <numeric>
+#include <queue>
+#include <functional>
 #include "bits.hpp"
 
 /*
  * Variable Length Retrieval with filter uses a filter at each node of the prefix free code
  * Note that the length of the filter bits might be zero meaning that no filter is used
+ *
+ * encoding is a two step procedure
+ * 1. construct the filter
+ * 2. create the code which consists of bits that correct the filter decisions
  */
 
 
 namespace learnedretrieval {
     struct FilterCode {
         using LengthType = uint64_t;
-        uint64_t correctedCode;
-        uint64_t filterCode;
-        LengthType codeLength;
-        LengthType correctionLength;
-        LengthType filterLength;
+        uint64_t code;
+        LengthType length;
+    };
+
+    class FilterLengthStrategyNoFilter {
+    public:
+        static uint64_t getFilterBits(float probability) {
+            return 0;
+        }
     };
 
     class FilterLengthStrategy1 {
+    public:
         static uint64_t getFilterBits(float probability) {
-            return 1;
+            if(probability<0.2){
+                return 2;
+            }
+            if(probability<0.3) {
+                return 1;
+            }
+            return 0;
+        }
+    };
+
+    template<typename Symbol = uint32_t, typename Frequency = float>
+    class FilterHuffmanCoder {
+    public:
+        struct Node {
+            Symbol s;
+            size_t n1;
+            size_t n2;
+            size_t parent;
+            bool bitRelParent;
+            Frequency p;
+            float relP;
+            size_t index;
+            bool leaf;
+        };
+        std::vector<Node> tree;
+        Node root;
+        Node currentDecodingNode;
+
+        class Compare {
+        public:
+            bool operator()(Node a, Node b) {
+                return a.p > b.p;
+            }
+        };
+
+
+        FilterHuffmanCoder(const std::span<Frequency> &f) {
+            std::priority_queue<Node, std::vector<Node>, Compare> nodes;
+            for (size_t i = 0; i < f.size(); ++i) {
+                Node n{i, 0, 0, 0, 0, f[i], 0, i, true};
+                nodes.push(n);
+                tree.push_back(n);
+            }
+            while (nodes.size() > 1) {
+                Node a = nodes.top();
+                nodes.pop();
+                a.bitRelParent = 0;
+                a.parent = tree.size();
+                tree[a.index] = a;
+                Node b = nodes.top();
+                nodes.pop();
+                b.bitRelParent = 1;
+                b.parent = tree.size();
+                tree[b.index] = b;
+                Node parent{0, a.index, b.index, 0, 0, a.p + b.p, a.p / (a.p + b.p), tree.size(), false};
+                /*std::cout << (a.p + b.p) << " " << parent.index << " " << a.index << " " << b.index << " " << a.p << " "
+                          << b.p << " " << parent.relP << std::endl;*/
+                tree.push_back(parent);
+                nodes.push(parent);
+            }
+            root = nodes.top();
+            currentDecodingNode = root;
+        }
+
+        std::vector<std::pair<float, bool>> getProbabilities(Symbol s) {
+            std::vector<std::pair<float, bool>> res;
+            Node current = tree[s];
+            while (current.index != root.index) {
+                bool b = current.bitRelParent;
+                current = tree[current.parent];
+                res.push_back({current.relP, b});
+            }
+            return res;
+        }
+
+        float getRelProbability() {
+            return currentDecodingNode.relP;
+        }
+
+        bool hasFinished() {
+            return currentDecodingNode.leaf;
+        }
+
+        void nextBit(bool bit) {
+            currentDecodingNode = tree[bit ? currentDecodingNode.n2 : currentDecodingNode.n1];
+        }
+
+        Symbol getResult() {
+            return currentDecodingNode.s;
         }
     };
 
     template<typename Coder, typename FilterLengthStrategy = FilterLengthStrategy1, typename Symbol = uint32_t, typename Frequency = float>
     class FilterCoding {
-        FilterCode encode_once(const std::span<Frequency> &f, Symbol symbol) {
+    public:
+        FilterCoding(size_t size) {
 
         }
 
-        Symbol decode_once(const std::span<Frequency> &f, const uint64_t *corrected_code_data, size_t &corrected_code_bit_offset, const uint64_t *filter_code_data, size_t &filter_code_bit_offset) {
+
+        /*
+         * get the filter code
+         * a 0 in the filter code is stored in a VLR retrieval structure, a 1 is skipped
+         */
+        FilterCode encode_once_filter(const std::span<Frequency> &f, Symbol symbol) {
+            Coder coder(f);
+            std::vector<std::pair<float, bool>> probs = coder.getProbabilities(symbol);
+            FilterCode res{0, 0};
+            for (size_t i = 0; i < probs.size(); ++i) {
+                auto [p, b] = probs[i];
+                uint64_t filterBits = FilterLengthStrategy::getFilterBits(p);
+                res.length += filterBits;
+                res.code <<= filterBits;
+                if (b) {
+                    res.code |= ((1 << filterBits) - 1);
+                }
+            }
+            return res;
+        }
+
+        FilterCode
+        encode_once_corrected_code(const std::span<Frequency> &f, Symbol symbol, uint64_t filter_code_data) {
+            Coder coder(f);
+            std::vector<std::pair<float, bool>> probs = coder.getProbabilities(symbol);
+
+            FilterCode res{0, 0};
+            for (size_t i = 0; i < probs.size(); ++i) {
+                auto [p, b] = probs[probs.size() - i - 1];
+
+                uint64_t filterBits = FilterLengthStrategy::getFilterBits(p);
+                uint64_t nodeFilterBits = filter_code_data & ((1 << filterBits) - 1);
+                filter_code_data >>= filterBits;
+
+                if (nodeFilterBits == 0) {
+                    res.code |= (b<<res.length);
+                    res.length++;
+                }
+
+            }
+            return res;
+        }
+
+        Symbol decode_once(const std::span<Frequency> &f, uint64_t corrected_code_data, uint64_t filter_code_data) {
+            // the coder has to ensure that at each node the probability of branch 0 is at most 50% (otherwise we would need to swap the filter)
             Coder coder(f);
             while (!coder.hasFinished()) {
-                float probability = coder.getNextProbability();
+                float probability = coder.getRelProbability();
+                assert(probability <= 0.5);
                 uint64_t filterBitLength = FilterLengthStrategy::getFilterBits(probability);
-                uint64_t filterBits = 0; //ToDo read next filter bits
-                if(filterBits) {
-                    coder.nextBit(true);
-                } else {
-                    bool nextBit = 0; // ToDo get next code bit
+                uint64_t filterBits = filter_code_data & ((1 << filterBitLength) - 1);
+                filter_code_data >>= filterBitLength;
+                if (filterBits == 0) {
+                    bool nextBit = corrected_code_data & 1;
                     coder.nextBit(nextBit);
+                    corrected_code_data >>= 1;
+                } else {
+                    coder.nextBit(true);
                 }
             }
             return coder.getResult();
--- ribbon_learned_filtered.cpp
+++ ribbon_learned_filtered.cpp
@@ -13,12 +13,12 @@
 #include <tlx/cmdline_parser.hpp>
 #include <tlx/logger.hpp>
 
-#include "learnedretrieval/huffman.hpp"
+#include "learnedretrieval/filter_coding.hpp"
 #include "learnedretrieval/dataset_reader.hpp"
 #include "learnedretrieval/model_wrapper.hpp"
 
 using ModelOutputType = float; // uint8
-
+#define LIMIT 300000
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         std::cerr << "Usage: " << argv[0] << " <dataset_path> <model_path>" << std::endl;
@@ -38,21 +38,47 @@ int main(int argc, char *argv[]) {
     using namespace ribbon;
     using Config = RConfig<64, 1, ThreshMode::onebit, false, true, false, 0, uint64_t>;
     //using Config = RConfig<64, 1, ThreshMode::onebit, false, true, false, 0, std::span<const float>>;
+    using FilerConfig = Config;
     IMPORT_RIBBON_CONFIG(Config);
     {
         learnedretrieval::ModelWrapper<ModelOutputType> model(model_path);
         rocksdb::StopWatchNano timer(true);
         size_t huffman_bits = 0;
+        size_t filter_bits = 0;
 
-        learnedretrieval::Huffman<uint64_t, ModelOutputType> coder(dataset.classes_count());
-        auto input = std::make_unique<std::pair<Key, ResultRowVLR>[]>(dataset.size());
-        //auto input = std::make_unique<std::pair<std::span<const float>, ResultRowVLR>[]>(dataset.size());
-        size_t maxlen = 0;
+        learnedretrieval::FilterCoding<learnedretrieval::FilterHuffmanCoder<>> coder(dataset.classes_count());
         auto state = XXH3_createState();
         assert(state);
+
+        size_t maxlenfilter = 0;
+        auto inputFilter = std::make_unique<std::pair<Key, ResultRowVLR>[]>(dataset.size());
+        for (size_t i = 0; i < dataset.size() && i<LIMIT; ++i) {
+            auto example = dataset.get_example(i);
+            auto label = dataset.get_label(i);
+            auto output = model.invoke(example);
+            auto probabilities = model.get_probabilities();
+            auto [code, filterLength] = coder.encode_once_filter(output, label);
+            XXH3_64bits_reset_withSeed(state, 100);
+            XXH3_64bits_update(state, &i, sizeof(size_t));
+            XXH3_64bits_update(state, example.data(), example.size_bytes());
+            auto hash = XXH3_64bits_digest(state);
+            inputFilter[i].first = hash;
+            if (filterLength > maxlenfilter)
+                maxlenfilter = filterLength;
+            inputFilter[i].second = static_cast<uint64_t>(code) | (uint64_t(1) << filterLength);
+            filter_bits += filterLength;
+        }
+
+
+        ribbon_filter<4, Config> filter(0.90625, 42, maxlenfilter);
+        filter.AddRange(inputFilter.get(), inputFilter.get() + LIMIT, false);
+        filter.BackSubst();
+
+        size_t maxlen = 0;
+        auto input = std::make_unique<std::pair<Key, ResultRowVLR>[]>(dataset.size());
         double entropy = 0;
         size_t numCorrectIsMoreThan50 = 0;
-        for (size_t i = 0; i < dataset.size(); ++i) {
+        for (size_t i = 0; i < dataset.size()&&i<LIMIT; ++i) {
             auto example = dataset.get_example(i);
             auto label = dataset.get_label(i);
             auto output = model.invoke(example);
@@ -64,24 +90,26 @@ int main(int argc, char *argv[]) {
             if (maxIdx == label && probabilities[label] > 0.5) {
                 ++numCorrectIsMoreThan50;
             }
-            auto [code, length] = coder.encode_once(output, label);
-            XXH3_64bits_reset_withSeed(state, 100);
-            XXH3_64bits_update(state, &i, sizeof(size_t));
-            XXH3_64bits_update(state, example.data(), example.size_bytes());
-            auto hash = XXH3_64bits_digest(state);
+            auto hash = inputFilter[i].first;
+
+            // get the output filter
+            uint64_t filterVal = filter.QueryRetrieval(hash);
+
+            size_t bit_offset = 0;
+            auto [code, length] = coder.encode_once_corrected_code(output, label, filterVal);
             input[i].first = hash;
-            //input[i].first = example;
             if (length > maxlen)
                 maxlen = length;
             input[i].second = static_cast<uint64_t>(code) | (uint64_t(1) << length);
             huffman_bits += length;
         }
+
         std::cout << "Entropy: " << (entropy / dataset.size()) << "\n";
         std::cout << "Correct guess has >50% prob: " << (numCorrectIsMoreThan50 / static_cast<double>(dataset.size())) << "\n";
         auto nanos = timer.ElapsedNanos(true);
         std::cout << "Preprocessing time: " << nanos << " ns (" << (nanos / static_cast<double>(dataset.size())) << " ns/item)\n";
         ribbon_filter<4, Config> r(0.90625, 42, maxlen);
-        r.AddRange(input.get(), input.get() + dataset.size());
+        r.AddRange(input.get(), input.get() + LIMIT);
         r.BackSubst();
         auto nanos2 = timer.ElapsedNanos(true);
         std::cout << "Ribbon construction time: " << nanos2 << " ns (" << (nanos2 / static_cast<double>(dataset.size())) << " ns/item)\n";
@@ -91,7 +119,7 @@ int main(int argc, char *argv[]) {
         auto model_bits = model.model_bytes() * 8;
 
         std::cout << "Max length: " << maxlen << "\n";
-        const size_t bytes = r.Size();
+        const size_t bytes = filter.Size();
         std::cout << "Ribbon size: " << (bytes * 8) << " bits\n";
         std::cout << "Ribbon bits/example: " << ((bytes * 8) / static_cast<double>(dataset.size())) << "\n";
         std::cout << "Model size: " << model_bits << " bits\n";
@@ -102,7 +130,7 @@ int main(int argc, char *argv[]) {
 
         volatile auto sum = 0;
         timer.Start();
-        for (size_t i = 0; i < dataset.size(); ++i) {
+        for (size_t i = 0; i < dataset.size() &&i<LIMIT; ++i) {
             auto example = dataset.get_example(i);
             auto label = dataset.get_label(i);
             auto output = model.invoke(example);
@@ -114,7 +142,7 @@ int main(int argc, char *argv[]) {
         timer.Start();
 
         bool ok = true;
-        for (size_t i = 0; i < dataset.size(); ++i) {
+        for (size_t i = 0; i < dataset.size()&&i<LIMIT; ++i) {
             auto example = dataset.get_example(i);
             auto label = dataset.get_label(i);
             auto output = model.invoke(example);
@@ -122,10 +150,9 @@ int main(int argc, char *argv[]) {
             XXH3_64bits_update(state, &i, sizeof(size_t));
             XXH3_64bits_update(state, example.data(), example.size_bytes());
             auto hash = XXH3_64bits_digest(state);
-            uint64_t val = r.QueryRetrieval(hash);
-            //ResultRowVLR val = r.QueryRetrieval(example);
-            size_t bit_offset = 0;
-            uint64_t res = coder.decode_once(output, &val, bit_offset);
+            uint64_t corrected_code = r.QueryRetrieval(hash);
+            uint64_t filterCode = filter.QueryRetrieval(hash);
+            uint64_t res = coder.decode_once(output, corrected_code, filterCode);
             bool found = res == label;
             assert(found);
             ok &= found;
