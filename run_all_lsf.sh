df17d4a7af59821efd3e6bd4f6545cb1e99f50e4
Stefan Hermann <stefan.a.hermann@gmail.com>
2025-07-23 14:03:05 +0200

    update script

--- run_all_lsf.sh
+++ run_all_lsf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir "out"
-(cd out && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ..)
+(cd out && cmake -DCMAKE_BUILD_TYPE=Release ..)
 (cd out && make)
-(cd out && ./plot_model_calibration > calibration.txt)
-(cd out && ./filter_tuner > filter.txt)
-(cd out && ./ribbon_learned_bench > bench.txt)
+(./out/plot_model_calibration > calibration.txt)
+(./out/filter_tuner > filter.txt)
+(./out/ribbon_learned_bench > bench.txt)
